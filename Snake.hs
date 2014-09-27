ea20dc4a045c71fd197e05760cc4359abbfa1df3
Becky Conning <becky.conning@gmail.com>
2014-09-27 19:06:10 +0100

    added random fruit, next: growing snake

--- Snake.hs
+++ Snake.hs
@@ -1,38 +1,71 @@
-import Control.Applicative
 import Data.Maybe
-import Control.Monad.Loops
+import Data.List
+
 import System.IO
-import System.Console.ANSI
-import Control.Concurrent
-import Control.Concurrent.Async
 import System.Timeout
 import System.Random
+import System.Console.ANSI
+
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Monad.Loops
+import Control.Applicative
 
 type Vector = (Int, Int)
 
-
+instance (Random x, Random y) => Random (x, y) where
+    random gen1 = ((x, y), gen3)
+        where (x, gen2) = random gen1
+              (y, gen3) = random gen2
+    randomR ((x1, y1), (x2, y2)) gen1 = ((x, y), gen3)
+        where (x, gen2) = randomR (x1, x2) gen1
+              (y, gen3) = randomR (y1, y2) gen2
 
 data State = State { 
     board    :: Int,
     snake    :: [Vector],
+    fruit    :: Maybe (Vector, StdGen),
     lastMove :: Maybe Vector
 } deriving Show
 
 sampleLength :: Int
-sampleLength = ((1*10^6) `div` 4)
+sampleLength = ((10 ^ 6) `div` 4)
 
-initialState :: State
-initialState = (State {
-    board    = 15,
-    snake    = [(4, 0), (3, 0), (2, 0), (1, 0), (0, 0)],
-    lastMove = Just (1, 0)
-})
+initialState :: IO State
+initialState = getStdGen >>= \stdGen ->
+    return (State {
+        board    = 15,
+        snake    = [(4, 0), (3, 0), (2, 0), (1, 0), (0, 0)],
+        fruit    = randomElem (foldl1 (++) (buildBoard 15)) stdGen, 
+        lastMove = Just (1, 0)
+    })
 
-main = iterateUntilM gameOver 
-                     step 
-                     initialState
+randomElem :: [a] -> StdGen -> Maybe (a, StdGen)
+randomElem [] stdGen  = Nothing
+randomElem xs stdGen  = Just (element, newStdGen)
+    where random    = randomR (0, (length xs - 1)) stdGen
+          index     = fst random
+          newStdGen = snd random
+          element   = xs !! index 
+
+newFruit :: State -> Maybe (Vector, StdGen)
+newFruit state@(State { fruit = Nothing }) = Nothing
+newFruit state@(State { fruit = Just (_, stdGen) })
+    = randomElem validPositions stdGen
+        where allPositions   = foldl1 (++) $ buildBoard $ board state
+              validPositions = allPositions \\ (snake state)
+
+updateFruit :: State -> Maybe (Vector, StdGen)
+updateFruit state@(State { snake = (snakeHead:_) })
+    | fruit state `fruitPositionEquals` snakeHead = newFruit state
+    | otherwise                                   = fruit state
+
+main = initialState >>= \ state ->
+    iterateUntilM gameOver 
+                  step 
+                  state
                      
-step :: State -> IO State 
+step :: State -> IO State
 step state = sample sampleLength getInput >>= \ move ->
         displayState $ updateState state (vectorFromChar move)
 
@@ -63,20 +96,26 @@ gameOver (State {
 render :: State -> String
 render state
     = unlines $ applyBorder (board state) 
-              $ map (renderRow (snake state)) 
+              $ map (renderRow state) 
               $ buildBoard (board state)
 
 applyBorder :: Int -> [[Char]] -> [[Char]]
 applyBorder size renderedRows 
     = border ++ map (\row -> ['X'] ++ row ++ ['X']) renderedRows ++ border
-    where border = [replicate (size + 2) 'X']
+        where border = [replicate (size + 2) 'X']
 
-renderRow :: [Vector] -> [Vector] -> String
-renderRow snakePositions = map (\cell -> snakeChar $ cell `elem` snakePositions)
+renderRow :: State -> [Vector] -> String
+renderRow state = map (characterForPosition state)
 
-snakeChar :: Bool -> Char
-snakeChar True  = '#'
-snakeChar False = ' '
+characterForPosition :: State -> Vector -> Char
+characterForPosition state position
+    | position `elem` snake state                = '#'
+    | fruit state `fruitPositionEquals` position = '@'
+    | otherwise                                  = ' '
+
+fruitPositionEquals :: Maybe (Vector, StdGen) -> Vector -> Bool
+fruitPositionEquals (Just (position, _)) vector = position == vector
+fruitPositionEquals _ _                         = False
 
 buildBoard :: Int -> [[(Int, Int)]]
 buildBoard size = [[(x, y) | x <- [0 .. size - 1]] | y <- reverse [0 .. size - 1]]
@@ -86,9 +125,10 @@ updateState state inputMove
     = (State {
         board = board state,
         snake = slither (snake state) validMove,
+        fruit = updateFruit state,
         lastMove = validMove
     })
-    where validMove = inputMove <|> (lastMove state)
+        where validMove = inputMove <|> (lastMove state)
 
 slither :: [Vector] -> Maybe Vector -> [Vector]
 slither snake (Just vector) = [(head snake) `vectorAdd` vector] ++ init snake
