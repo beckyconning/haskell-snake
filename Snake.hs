58d7c12f97606577c1291bb4e3b7ee4596690d6f
Becky Conning <becky.conning@gmail.com>
2014-09-28 03:51:57 +0100

    removed trailing whitespace

--- Snake.hs
+++ Snake.hs
@@ -14,7 +14,7 @@ import Control.Applicative
 
 type Vector = (Int, Int)
 
-data State = State { 
+data State = State {
     board :: Int,
     snake :: [Vector],
     fruit :: Maybe (Vector, StdGen),
@@ -32,7 +32,7 @@ initialState = getStdGen >>= \stdGen ->
     return State {
         board = 15,
         snake = [(4, 0), (3, 0), (2, 0), (1, 0), (0, 0)],
-        fruit = randomElem (concat (buildBoard 15)) stdGen, 
+        fruit = randomElem (concat (buildBoard 15)) stdGen,
         move  = Just (1, 0)
     }
 
@@ -42,7 +42,7 @@ randomElem xs inputStdGen  = Just (element, stdGen)
     where indexStdGenTuple = randomR (0, length xs - 1) inputStdGen
           index            = fst indexStdGenTuple
           stdGen           = snd indexStdGenTuple
-          element          = xs !! index 
+          element          = xs !! index
 
 newFruit :: State -> Maybe (Vector, StdGen)
 newFruit (State { fruit = Nothing }) = Nothing
@@ -52,10 +52,10 @@ newFruit state@(State { fruit = Just (_, stdGen) })
               validPositions = allPositions \\ snake state
 
 main :: IO State
-main = initialState >>= \ state -> iterateUntilM gameOver 
-                                                 step 
+main = initialState >>= \ state -> iterateUntilM gameOver
+                                                 step
                                                  state
-                     
+               
 step :: State -> IO State
 step state = sample sampleLength getInput >>= \ inputMove ->
         displayState $ updateState state (vectorFromChar inputMove)
@@ -71,12 +71,12 @@ vectorFromChar (Just 'd') = Just ( 1,  0)
 vectorFromChar _          = Nothing
 
 getInput :: IO Char
-getInput = hSetEcho stdin False >> hSetBuffering stdin NoBuffering 
+getInput = hSetEcho stdin False >> hSetBuffering stdin NoBuffering
                                 >> getChar
 
 gameOver :: State -> Bool
 gameOver (State { snake = [] }) = True
-gameOver (State { 
+gameOver (State {
     board = boardSize,
     snake = (snakeHead@(snakeHeadX, snakeHeadY):snakeBody)
 })
@@ -87,12 +87,12 @@ gameOver (State {
 
 render :: State -> String
 render state
-    = unlines $ applyBorder (board state) 
-              $ map (renderRow state) 
+    = unlines $ applyBorder (board state)
+              $ map (renderRow state)
               $ buildBoard (board state)
 
 applyBorder :: Int -> [String] -> [String]
-applyBorder size renderedRows 
+applyBorder size renderedRows
     = border ++ map (\row -> "X" ++ row ++ "X") renderedRows ++ border
         where border = [replicate (size + 2) 'X']
 
@@ -110,41 +110,41 @@ fruitPositionEquals (Just (position, _)) vector = position == vector
 fruitPositionEquals _ _                         = False
 
 snakeHasFruitInMouth :: State -> Bool
-snakeHasFruitInMouth state 
+snakeHasFruitInMouth state
     = fruit state `fruitPositionEquals` head (snake state)
 
 buildBoard :: Int -> [[(Int, Int)]]
-buildBoard size 
+buildBoard size
     = [[(x, y) | x <- [0 .. size - 1]] | y <- reverse [0 .. size - 1]]
 
-updateState :: State -> Maybe Vector -> State 
-updateState state inputMove 
+updateState :: State -> Maybe Vector -> State
+updateState state inputMove
     = updateFruit $ updateSnake $ updateMove state inputMove
 
 updateMove :: State -> Maybe Vector -> State
 updateMove state@(State { move = Just vector }) inputMove@(Just inputVector)
-    | inputVector == vectorOpposite vector 
+    | inputVector == vectorOpposite vector
         = state
-    | otherwise                            
+    | otherwise
         = state { move = inputMove <|> move state }
 updateMove state _ = state
 
 updateSnake :: State -> State
 updateSnake = updateSnakeTail . updateSnakeHead
 
-updateFruit :: State -> State 
+updateFruit :: State -> State
 updateFruit state
     | snakeHasFruitInMouth state = state { fruit = newFruit state }
-    | otherwise                  = state 
+    | otherwise                  = state
 
-updateSnakeHead :: State -> State 
+updateSnakeHead :: State -> State
 updateSnakeHead state@(State { move = (Just vector) })
     = state { snake = head (snake state) `vectorAdd` vector : snake state }
 updateSnakeHead state = state
 
 updateSnakeTail :: State -> State
 updateSnakeTail state
-    | snakeHasFruitInMouth state = state 
+    | snakeHasFruitInMouth state = state
     | otherwise                  = state { snake = init $ snake state }
 
 vectorAdd :: Vector -> Vector -> Vector
@@ -157,6 +157,6 @@ sample :: Int -> IO a -> IO (Maybe a)
 sample n f
     | n <  0    = fmap Just f
     | n == 0    = return Nothing
-    | otherwise = 
+    | otherwise =
         concurrently (timeout n f) (threadDelay n) >>= \ (result, _) ->
             return result
